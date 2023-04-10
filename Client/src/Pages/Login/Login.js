8ad3ccdeb73336fa680459fcc434b700f1937316
DanielBedrack <danielbedrack@gmail.com>
2023-04-10 20:16:43 +0300

    Adding signup validation and token

--- Client/src/Pages/Login/Login.js
+++ Client/src/Pages/Login/Login.js
@@ -1,11 +1,22 @@
-import React from 'react'
+//import React, { useEffect, useState } from 'react'
 import LoginForm from '../../Components/Forms/Login'
 //import RegisterForm from '../../Components/Forms/Registration'
 import './login.css'
 
 const Login = () => {
+  // const [users, setUsers] = useState(null)
 
-  
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const response = await fetch('http://localhost:4000/api/users')
+  //     const json = await response.json()
+
+  //     if(response.ok){
+  //       setUsers(json)
+  //     }
+  //   }
+  //   fetchUsers()
+  // }, [])
   return (
     <div className='login'>
       <LoginForm />
